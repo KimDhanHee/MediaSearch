5553ecaec519b0f247fbdbd8958783330d6dc7e3
Kim Dan Hee <sgoiboy@gmail.com>
2023-03-19 16:44:29 +0900

    api_key, base_url buildConfig 선언

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
   kotlin("kapt")
   id("com.android.library")
@@ -9,6 +11,19 @@ plugins {
 android {
   namespace = "com.danytothemoon.core.network"
   compileSdk = 33
+
+  defaultConfig {
+    buildConfigField(
+      type = "String",
+      name = "KAKAO_API_KEY",
+      value = gradleLocalProperties(rootDir).getProperty("kakao_api_key")
+    )
+    buildConfigField(
+      type = "String",
+      name = "KAKAO_API_BASE_URL",
+      value = gradleLocalProperties(rootDir).getProperty("kakao_api_base_url")
+    )
+  }
 
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_1_8
