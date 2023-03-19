69795df948721aab52b94d0393454c830e0df385
Kim Dan Hee <sgoiboy@gmail.com>
2023-03-19 19:59:16 +0900

    main activity xml

--- app/src/main/java/com/danytothemoon/searchmedia/MainActivity.kt
+++ app/src/main/java/com/danytothemoon/searchmedia/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.danytothemoon.searchmedia
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.databinding.DataBindingUtil
+import com.danytothemoon.searchmedia.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity()
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+  }
+}
