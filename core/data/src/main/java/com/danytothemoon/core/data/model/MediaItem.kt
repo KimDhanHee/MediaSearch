84a24731d3a8441b625fd5968a480d3921e7c3a4
Kim Dan Hee <sgoiboy@gmail.com>
2023-03-20 15:46:51 +0900

    dateStr, timeStr 선언

--- core/data/src/main/java/com/danytothemoon/core/data/model/MediaItem.kt
+++ core/data/src/main/java/com/danytothemoon/core/data/model/MediaItem.kt
@@ -1,8 +1,16 @@
 package com.danytothemoon.core.data.model
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.toJavaLocalDateTime
+import java.time.format.DateTimeFormatter
 
 data class MediaItem(
   val url: String,
   val datetime: LocalDateTime,
-)
+) {
+  val dateStr: String = datetime.format("yyyy-MM-dd")
+  val timeStr: String = datetime.format("HH:mm:ss")
+}
+
+fun LocalDateTime.format(pattern: String) =
+  this.toJavaLocalDateTime().format(DateTimeFormatter.ofPattern(pattern))
