a10d52ccda1202cfe5f029e6cf4e924a1bca7016
Kim Dan Hee <sgoiboy@gmail.com>
2023-03-20 20:55:49 +0900

    search 함수 병렬 요청

--- core/data/src/main/java/com/danytothemoon/core/data/repository/MediaRepositoryImpl.kt
+++ core/data/src/main/java/com/danytothemoon/core/data/repository/MediaRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.danytothemoon.core.data.model.MediaItem
 import com.danytothemoon.core.network.retrofit.SearchMediaNetwork
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
@@ -11,14 +12,16 @@ import javax.inject.Inject
 class MediaRepositoryImpl @Inject constructor(
   private val network: SearchMediaNetwork,
 ) : MediaRepository {
-  override fun search(keyword: String): Flow<List<MediaItem>> = flow {
-    val videos = network.getVideos(keyword).documents.map {
-      MediaItem(url = it.url, datetime = it.datetime)
+  override fun search(keyword: String): Flow<List<MediaItem>> = combine(
+    flow {
+      val videos = network.getVideos(keyword).documents.map { MediaItem(it.url, it.datetime) }
+      emit(videos)
+    },
+    flow {
+      val images = network.getImages(keyword).documents.map { MediaItem(it.url, it.datetime) }
+      emit(images)
     }
-    val images = network.getImages(keyword).documents.map {
-      MediaItem(url = it.url, datetime = it.datetime)
-    }
-    val result = (videos + images).sortedByDescending { it.datetime }
-    emit(result)
+  ) { videos, images ->
+    (videos + images).sortedByDescending { it.datetime }
   }.flowOn(Dispatchers.IO)
 }
