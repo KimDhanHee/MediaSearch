ea16c39862a5707bb18cffb62bd30c50c3f71d77
김단희 <sgoiboy@gmail.com>
2023-03-25 08:01:48 +0900

    빈 관심 등록

--- feature/interests/src/main/java/com/danytothemoon/feature/interests/InterestsFragment.kt
+++ feature/interests/src/main/java/com/danytothemoon/feature/interests/InterestsFragment.kt
@@ -4,9 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BookmarkAdd
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.danytothemoon.core.designsystem.theme.SearchMediaTheme
@@ -24,8 +38,29 @@ class InterestsFragment : Fragment() {
       SearchMediaTheme {
         val viewmodel: InterestsViewModel = viewModel()
         val mediaItems by viewmodel.interestedMediaListFlow.collectAsState()
-        MediaItemGridList(mediaItems, onClickItem = viewmodel::deregisterInterest)
+
+        when {
+          mediaItems.isEmpty() -> EmptyInterest()
+          else -> MediaItemGridList(mediaItems, onClickItem = viewmodel::deregisterInterest)
+        }
       }
+    }
+  }
+
+  @Composable
+  private fun EmptyInterest() {
+    Column(
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Icon(
+        imageVector = Icons.Default.BookmarkAdd,
+        contentDescription = null,
+        modifier = Modifier.size(96.dp),
+        tint = Color.Gray
+      )
+      Spacer(modifier = Modifier.size(24.dp))
+      Text(text = stringResource(id = R.string.empty_interest))
     }
   }
 }
